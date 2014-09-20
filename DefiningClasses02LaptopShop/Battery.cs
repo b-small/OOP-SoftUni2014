b624f927220d3425b4b192904218ecbad65d6ba3
Blagovest <sblagovesta@yahoo.com>
2014-09-20 14:52:09 +0300

    battery changes

--- DefiningClasses02LaptopShop/Battery.cs
+++ DefiningClasses02LaptopShop/Battery.cs
@@ -1,12 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DefiningClasses02LaptopShop
 {
     class Battery
     {
+        private BatteryType type;
+        private short cells;
+        private ushort capacity;
+
+        public Battery(BatteryType t, short c, ushort ca)
+        {
+            this.type = t;
+            this.Cells = c;
+            this.Capacity = ca;
+        }
+
+        public short Cells
+        {
+            get { return this.cells; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("False cells value");
+                }
+                this.cells = value;
+            }
+        }
+
+        public ushort Capacity
+        {
+            get { return this.capacity; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("False capacity value");
+                }
+                this.capacity = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}-cells, {2} mAh", this.type, this.cells, this.capacity);
+        }
     }
 }
--- DefiningClasses02LaptopShop/BatteryType.cs
+++ DefiningClasses02LaptopShop/BatteryType.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DefiningClasses02LaptopShop
 {
-    class BatteryType
+    public enum BatteryType
     {
+        NiCad,
+        NiMH,
+        Lion,
+        LiPo
     }
 }
