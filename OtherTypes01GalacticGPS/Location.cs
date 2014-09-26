4d50287733857adc16f985c5a556005ff354cd6e
Blagovest <sblagovesta@yahoo.com>
2014-09-26 18:56:05 +0300

    3-01

---
+++ OtherTypes01GalacticGPS/Location.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OtherTypes01GalacticGPS
+{
+    public struct Location
+    {
+        private double latitude;
+        private double longitude;
+        private Planet currentPlanet;
+
+        public Location(double latitude, double longitude, Planet p) : this()
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+            this.CurrentPlanet = p;
+        }
+
+        public double Latitude
+        {
+            get { return this.latitude; }
+            set { this.latitude = value; }
+        }
+
+        public double Longitude
+        {
+            get { return this.longitude; }
+            set { this.longitude = value; }
+        }
+
+        public Planet CurrentPlanet
+        {
+            get { return this.currentPlanet; }
+            set { this.currentPlanet = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1} - {2}", this.Latitude, this.Longitude, this.CurrentPlanet);
+        }
+
+    }
+}
---
+++ OtherTypes01GalacticGPS/LocationTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OtherTypes01GalacticGPS
+{
+    class LocationTest
+    {
+        static void Main(string[] args)
+        {
+            Location l0 = new Location(18.037986, 28.870097, Planet.Earth);
+            Location l1 = new Location(-4, -243.645, Planet.Venus);
+
+            Console.WriteLine(l0);
+            Console.WriteLine(l1);
+        }
+    }
+}
