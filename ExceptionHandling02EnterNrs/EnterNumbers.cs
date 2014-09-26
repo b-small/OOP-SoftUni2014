aa3aeb77b4d753c5119e65ba65b0bad7e68131ac
Blagovest <sblagovesta@yahoo.com>
2014-09-26 18:19:16 +0300

    task 02

---
+++ ExceptionHandling02EnterNrs/EnterNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExceptionHandling02EnterNrs
+{
+    class EnterNumbers
+    {
+        public static void ReadNumbers(int start, int end)
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                int num = int.Parse(input);
+                if (num <= start || num >= end)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid number! Try again.");
+                ReadNumbers(start, end);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+                for (int i = 0; i < 10; i++)
+                {
+                    ReadNumbers(1, 100);
+                }
+        }
+    }
+}
