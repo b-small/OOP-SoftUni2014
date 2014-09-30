bc5ab83da045539f52f1d89e386ea8be061791c1
Blagovest <sblagovesta@yahoo.com>
2014-09-30 22:45:20 +0300

    hw5-02

---
+++ FuncProgramming02LINQExtensions/LINQExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncProgramming02LINQExtensions
+{
+    static class LINQExtensions
+    {
+        public static IEnumerable<T> WhereNot<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            return collection.Where(a => !predicate(a));
+        }
+
+        public static IEnumerable<T> Repeat<T>(this IEnumerable<T> collection, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                foreach (var item in collection)
+                {
+                    yield return item;
+                }
+            } 
+        }
+
+        public static IEnumerable<string> WhereEndsWith(this IEnumerable<string> collection, IEnumerable<string> suffixes)
+        {
+            var res = new List<string>();
+            foreach (var item in collection)
+            {
+                foreach (var suffix in suffixes)
+                {
+                    if (item.EndsWith(suffix))
+                    {
+                        res.Add(item);
+                    }
+                }
+
+            }
+            return res;
+        }
+    }
+}
---
+++ FuncProgramming02LINQExtensions/LINQExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncProgramming02LINQExtensions
+{
+    class LINQExtensionsTest
+    {
+        static void Main()
+        {
+            var numbers = new List<int>() { 2, 4, 6, 22, 13, 11, 5754, 245 };
+
+            Console.WriteLine(string.Join(" ", numbers.WhereNot(a => a % 2 == 0)));
+
+            Console.WriteLine(string.Join(" ", numbers.Repeat<int>(2)));
+
+            IEnumerable<string> stringItems = new List<string>() { "Jivei", "Spokoino", ",", "gledai", "samo", "napred", "Hakuna", "matata" };
+            IEnumerable<string> suffixes = new List<string>() { "ei", "ai", ","};
+            Console.WriteLine(string.Join(" ", stringItems.WhereEndsWith(suffixes)));
+        }
+    }
+}
