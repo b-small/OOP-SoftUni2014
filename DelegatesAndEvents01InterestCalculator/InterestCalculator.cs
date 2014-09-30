5cb016febba583c3ff6dc8d586c4abeb94ab472b
Blagovest <sblagovesta@yahoo.com>
2014-09-30 18:34:06 +0300

    hw4

---
+++ DelegatesAndEvents01InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace DelegatesAndEvents01InterestCalculator
+{
+    public delegate double CalculateInterest(double sum, double interest, int years);
+
+    class InterestCalculator
+    {
+        private double sum;
+        private double interest;
+        private int years;
+        private CalculateInterest ci;
+
+        public InterestCalculator(double s, double i, int y, CalculateInterest type)
+        {
+            this.sum = s;
+            this.interest = i;
+            this.years = y;
+            this.ci = type;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0:F4}", this.ci(this.sum, this.interest, this.years));
+        }
+    }
+}
---
+++ DelegatesAndEvents01InterestCalculator/InterestCalculatorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents01InterestCalculator
+{
+    class InterestCalculatorTest
+    {
+
+        public static double GetSimpleInterest(double sum, double interest, int years)
+        {
+            double si = sum * (double)(1 + (interest / 100) * years);
+            return si;
+        }
+
+        public static double GetCompoundInterest(double sum, double interest, int years)
+        {
+            double ci = sum * Math.Pow(1 + ((interest / 100) / 12), years * 12);
+            return ci;
+        }
+
+        static void Main(string[] args)
+        {
+            InterestCalculator ic = new InterestCalculator(500, 5.6, 10, GetCompoundInterest);
+            Console.WriteLine(ic);
+            ic = new InterestCalculator(2500, 7.2, 15, GetSimpleInterest);
+            Console.WriteLine(ic);
+        }
+    }
+}
